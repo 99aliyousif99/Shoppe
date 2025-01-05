c6625434a56d675f8a12f6b192ca40d64b2cb9ff
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-05 14:24:22 +0300

    add CreateAccount component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 // import Intro from "./pages/Intro/Intro";
-
+import CreateAccount from "./pages/CreateAccount/CreateAccount"
 
 function App() {
   return (
     <>
      {/* <Intro/> */}
+     <CreateAccount/>
     </>
   );
 }
