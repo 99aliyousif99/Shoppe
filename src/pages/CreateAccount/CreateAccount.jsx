a57ee7c18f332d4c906981990cef0eef5f5bab7b
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-05 14:43:39 +0300

    commit

--- src/pages/CreateAccount/CreateAccount.jsx
+++ src/pages/CreateAccount/CreateAccount.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import "./CreateAccount.css"
+import React from "react";
+import "./CreateAccount.css";
+import uploadPhoto from "../../assets/upload photo.svg";
+
 const CreateAccount = () => {
   return (
-    <div>CreateAccount</div>
-  )
-}
+    <>
+      <div className="signUp">
+        <h1>Create</h1>
+        <h1>Account</h1>
+        <div className="upload">
+         
+          <img src={uploadPhoto} alt="Upload Photo" />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default CreateAccount
+export default CreateAccount;
