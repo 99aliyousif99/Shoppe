87007acfcc18a1a34fb099f6a4503a8101a0988a
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-07 15:22:44 +0300

    commit

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,7 +5,10 @@ const Main = () => {
   return (
     <>
     <div className="main">
-        main
+        <div className="top">
+            <h1>Shop</h1>
+            <input type="text" placeholder='clothing X' />
+        </div>
     </div>
     </>
   )
