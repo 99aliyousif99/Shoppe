0f65cbc1bc00fbdcb4fe597d7cbe54aa98ed25e8
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-07 11:16:31 +0300

    replace CreateAccount component with Login component in App and add Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 // import Intro from "./pages/Intro/Intro";
-import CreateAccount from "./pages/CreateAccount/CreateAccount"
-
+// import CreateAccount from "./pages/CreateAccount/CreateAccount"
+import Login from "./pages/Login/Login";
 function App() {
   return (
     <>
      {/* <Intro/> */}
-     <CreateAccount/>
+     {/* <CreateAccount/> */}
+     <Login/>
     </>
   );
 }
