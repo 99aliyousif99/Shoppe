28838f56a3dac8831c477b502e0ff11162019551
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-07 12:30:40 +0300

    commit

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
 import "./login.css";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <div className="login">
+        <h1>Login</h1>
+        <p>Good to see you back!</p>
+        <input type="text" placeholder="Email" />
+        <button>Next</button>
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
