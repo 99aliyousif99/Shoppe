942f9eebe3d3c89e7ef33902c43390edcbfd5e1f
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-08 14:18:49 +0300

    enhance ProductCard component with image and description

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import Image from "../../assets/ProductImage.png";
 const ProductCard = () => {
   return (
-    <div>ProductCard</div>
-  )
-}
+    <>
+      <div className="card">
+        <div className="image">
+          <img src={Image} alt="" />
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur</p>
+        <span>17 $</span>
+      </div>
+    </>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
