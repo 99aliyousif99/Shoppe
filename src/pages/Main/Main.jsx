252fbf3a6becf6d277e7562faa056915acda762c
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-08 13:37:17 +0300

    commit

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React from "react";
 import "./main.css";
-
+import bagsImage from "../../assets/categories/bags.png";
+import categoriesData from "../../Data/categories.json";
+import Filter from "../../assets/filter.svg"
 const Main = () => {
   return (
     <>
-    <div className="main">
+      <div className="main">
         <div className="top">
-            <h1>Shop</h1>
-            <input type="text" placeholder='clothing X' />
+          <h1>Shop</h1>
+          <input type="text" placeholder="clothing X" />
         </div>
-    </div>
-    </>
-  )
-}
+        <div className="categories">
+          {categoriesData.categories.map((category) => (
+            <>
+              <div key={category.id} className="category">
+                <img src={category.image} alt={category.name} />
+              </div>
+            </>
+          ))}
+        </div>
 
-export default Main
+        <div className="items">
+          <h2>All Items</h2>
+          <img src={Filter} alt="" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
