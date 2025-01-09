d1c838694f4bcff1f087bf39c17f60dd81ba9545
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-09 11:33:59 +0300

    refactor NavBar component: update styles and add icon selection functionality

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
-import "./NavBar.css"
-import { FiHome ,FiShoppingBag} from "react-icons/fi";
+import React, { useState } from 'react';
+import "./NavBar.css";
+import { FiHome, FiShoppingBag } from "react-icons/fi";
 import { CiHeart } from "react-icons/ci";
 import { RxAvatar } from "react-icons/rx";
 import { BsFilterSquare } from "react-icons/bs";
 
 const NavBar = () => {
-  return (
-    <>
-    <div className="navBar">a</div>
-    </>
-  )
-}
+  const [selectedIcon, setSelectedIcon] = useState(null);
 
-export default NavBar
+  const handleIconClick = (icon) => {
+    setSelectedIcon(icon);
+  };
+
+  return (
+    <div className="navBar">
+      <FiHome
+        size={30}
+        color={selectedIcon === 'home' ? 'black' : '#004CFF'}
+        onClick={() => handleIconClick('home')}
+      />
+      <FiShoppingBag
+        size={30}
+        color={selectedIcon === 'shoppingBag' ? 'black' : '#004CFF'}
+        onClick={() => handleIconClick('shoppingBag')}
+      />
+      <CiHeart
+        size={30}
+        color={selectedIcon === 'heart' ? 'black' : '#004CFF'}
+        onClick={() => handleIconClick('heart')}
+      />
+      <RxAvatar
+        size={30}
+        color={selectedIcon === 'avatar' ? 'black' : '#004CFF'}
+        onClick={() => handleIconClick('avatar')}
+      />
+      <BsFilterSquare
+        size={30}
+        color={selectedIcon === 'filter' ? 'black' : '#004CFF'}
+        onClick={() => handleIconClick('filter')}
+      />
+    </div>
+  );
+};
+
+export default NavBar;
