8ad9fee5e677af0e8f53d3b5ac2e7a928804f1c2
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-19 12:49:08 +0300

    commit

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -5,8 +5,16 @@ const Product = ({ product, onClose }) => {
   console.log(product);
 
   return (
-    <div className="product">
-      <div>Product: {product.name}</div>
+    <div className="prod">
+      <div className="imageCover">
+        <img src={product.image} alt="" />
+      </div>
+      <div className="options">
+        <p>color options</p>
+         <div className="colors">
+          <img src={product.subImages[1]} alt="" />
+         </div>
+      </div>
     </div>
   );
 };
