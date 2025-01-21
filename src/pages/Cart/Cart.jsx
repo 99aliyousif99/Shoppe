94ba2e62c902e1b6dc099fe6881fd86b0dd85b93
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-21 11:59:05 +0300

    feat: implement Cart component layout with styling adjustments

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import "./cart.css";
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <>
+    <div className="cart">
+      <h1>Cart</h1>
+
+    </div>
+    
+    </>
   )
 }
 
