18b94cde70a38c1fd3298b98513b9be651dd7f60
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-21 12:38:14 +0300

    feat: add price to products and enhance Cart component layout with shipping address and edit functionality

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React from "react";
 import "./cart.css";
+import { MdOutlineEdit } from "react-icons/md";
 
 const Cart = () => {
   return (
     <>
-    <div className="cart">
-      <h1>Cart</h1>
-
-    </div>
-    
+      <div className="cart">
+        <h1>Cart</h1>
+        <div className="shipping">
+          <span>shippig address</span>
+          <div className="location">
+            <p>
+              26, Duong So 2, Thao Dien Wardsssssssssssssss
+            </p>
+            <button className="editAddress">
+              <MdOutlineEdit />
+            </button>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
