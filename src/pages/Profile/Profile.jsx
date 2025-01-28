31fc758e83316a8701f8a17cb5a244765a10aa66
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-28 11:14:28 +0300

    feat: update Profile component layout and add settings icons

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./profile.css";
-import avatar from "../../assets/avatar.png"
+import avatar from "../../assets/avatar.png";
+import { CiSettings } from "react-icons/ci";
+import { LuScanLine } from "react-icons/lu";
+import { BsTextRight } from "react-icons/bs";
+
 const Profile = () => {
   return (
     <>
@@ -12,7 +16,15 @@ const Profile = () => {
           <h2>Vouchers</h2>
         </div>
         <div className="settings">
-          
+          <div className="container">
+            <LuScanLine />
+          </div>
+          <div className="container">
+            <CiSettings />
+          </div>
+          <div className="container">
+            <BsTextRight />
+          </div>
         </div>
       </div>
     </>
