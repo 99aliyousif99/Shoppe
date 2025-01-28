96638192113d1b83037aeac818fd033bd070780f
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-28 10:31:46 +0300

    commit

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import "./profile.css";
+import avatar from "../../assets/avatar.png"
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <>
+      <div className="header">
+        <div className="name">
+          <div className="avatar">
+            <img src={avatar} alt="" />
+          </div>
+          <h2>Vouchers</h2>
+        </div>
+        <div className="settings">
+          
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Profile
+export default Profile;
