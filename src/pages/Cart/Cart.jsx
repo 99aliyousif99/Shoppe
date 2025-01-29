fddd04993b7f06477c51c894f50c8b2921957b94
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-29 13:06:12 +0300

    feat: enhance Cart component with improved total section styling and layout

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -36,41 +36,43 @@ const Cart = () => {
 
   return (
     <>
-    <div className="cart">
-      <h1>Cart</h1>
-      <div className="shipping">
-        <span>Shipping address</span>
-        <div className="location">
-          <p>26, Duong So 2, Thao Dien Ward</p>
-          <button className="editAddress">
-            <MdOutlineEdit />
-          </button>
-        </div>
-      </div>
-      {cart.map((product) => (
-        <div key={product.id} className="cartProduct">
-          <div className="image">
-            <img src={product.image} alt={product.name} />
+      <div className="cart">
+        <h1>Cart</h1>
+        <div className="shipping">
+          <span>Shipping address</span>
+          <div className="location">
+            <p>26, Duong So 2, Thao Dien Ward</p>
+            <button className="editAddress">
+              <MdOutlineEdit />
+            </button>
           </div>
-          <div className="info">
-            <p className="name">{product.name}</p>
-            <p className="size">size {product.size}</p>
-            <div className="price">
-              <p>${(product.price * quantities[product.id]).toFixed(2)}</p>
-              <QuantitySelector
-                initialQuantity={quantities[product.id]}
-                onQuantityChange={(newQuantity) =>
-                  handleQuantityChange(product.id, newQuantity)
-                }
-                size="small"
-              />
+        </div>
+        {cart.map((product) => (
+          <div key={product.id} className="cartProduct">
+            <div className="image">
+              <img src={product.image} alt={product.name} />
+            </div>
+            <div className="info">
+              <p className="name">{product.name}</p>
+              <p className="size">size {product.size}</p>
+              <div className="price">
+                <p>${(product.price * quantities[product.id]).toFixed(2)}</p>
+                <QuantitySelector
+                  initialQuantity={quantities[product.id]}
+                  onQuantityChange={(newQuantity) =>
+                    handleQuantityChange(product.id, newQuantity)
+                  }
+                  size="small"
+                />
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-      
-    </div>
-    <div className="total"></div>
+        ))}
+      </div>
+      <div className="total">
+        <p>total</p>
+        <button>checkout</button>
+      </div>
     </>
   );
 };
