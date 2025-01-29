676dcfcfbe0fc8363366e73a468a1d1073b3c366
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-29 13:16:19 +0300

    feat: update QuantitySelector component with icon buttons and adjust font size

--- src/components/QuantitySelector/QuantitySelector.jsx
+++ src/components/QuantitySelector/QuantitySelector.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./QuantitySelector.css";
+import { FaMinus } from "react-icons/fa6";
+import { FaPlus } from "react-icons/fa6";
 
 const QuantitySelector = ({ initialQuantity, onQuantityChange, size }) => {
   const [quantity, setQuantity] = useState(initialQuantity || 1);
@@ -23,9 +25,11 @@ const QuantitySelector = ({ initialQuantity, onQuantityChange, size }) => {
   return (
     <div className={`quantity ${size}`}>
       <div className="amount">
-        <div className="decrease" onClick={handleDecrease}>-</div>
+        <div className="decrease" onClick={handleDecrease}><FaMinus />
+        </div>
         <div className="num">{quantity}</div>
-        <div className="increase" onClick={handleIncrease}>+</div>
+        <div className="increase" onClick={handleIncrease}><FaPlus />
+        </div>
       </div>
     </div>
   );
