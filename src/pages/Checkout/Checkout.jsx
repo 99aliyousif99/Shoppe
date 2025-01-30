c4bf11dfcacf07c534176cb42e61203023e86a8a
99aliyousif99 <aljanabiali3@gmail.com>
2025-01-30 11:39:56 +0300

    feat: enhance Checkout component layout with voucher section and style adjustments

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -5,6 +5,7 @@ import Address from "../../components/Address/Address";
 
 const Checkout = () => {
   const cart = useCartStore((state) => state.cart);
+ 
   console.log(cart)
   const totalValue =()=>{
     let total =0
@@ -19,7 +20,15 @@ const Checkout = () => {
       <div className="payment">
         <h1>payment</h1>
         <Address />
-
+        <div className="products">
+          <div className="vouchers">
+            <div className="ems">
+            <h2>items</h2>
+            <div className="quan">{cart.length}</div>
+            </div>
+            <button>Add Voucher</button>
+          </div>
+        </div>
       </div>
     
     </>
